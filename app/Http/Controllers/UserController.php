973668ff2ae353d2a0d003fde5bf60f7107842e9
Zoe31303133 <ch10161016@gmail.com>
2024-03-23 22:45:36 +0800

    返回全部，create()，store()，show()

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\User;
+use App\Http\Controllers\RoleController;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return 
+        ['roleList' =>(new RoleController)->index()];
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        
+        $registerUser  = $request->user;
+
+        $user = new User();
+        $user->name = $registerUser->name;
+        $user->email = $registerUser->email;
+        $user->password = $registerUser->password;
+        $user->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $user = User::findOrfail($id);
+        return $user;        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+}
