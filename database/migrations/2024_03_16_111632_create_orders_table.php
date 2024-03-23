40ebfa4033b23705ece17f9c43bb8ec3db8a7de9
Zoe31303133 <ch10161016@gmail.com>
2024-03-23 22:26:31 +0800

    為了order_item從order抽出，改動order欄位

--- database/migrations/2024_03_16_111632_create_orders_table.php
+++ database/migrations/2024_03_16_111632_create_orders_table.php
@@ -12,13 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->char('id', 25);
-            $table->char('product_id', 25);
-            $table->decimal('quantity', $precision = 5);
+            $table->integer('id', 5);
             $table->date('date');
             $table->time('time');
-            
-            $table->primary(['id', 'product_id']);
+            $table->char('state', 25);
         });
     }
 
