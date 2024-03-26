1cacf6f0662a88b88fb8b52498c4a5f59bf50b54
Zoe31303133 <ch10161016@gmail.com>
2024-03-26 23:45:41 +0800

    上方navBar與sideBar版型完成

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,21 +1,25 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import ManagerSideBar from '@/Components/SideBars/manager';
 import { Head } from '@inertiajs/react';
+import '../../../resources/bootstrap_module/sidebars/sidebars.js';
 
 export default function Dashboard({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+        }
         >
             <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+            
+            <main className='d-flex flex-nowrap'>
+                <ManagerSideBar></ManagerSideBar>
+                <div class="d-flex gap-3 small mt-3">
+                    <div className="col-md-9  ms-sm-auto col-lg-10 px-md-4 overflow-scroll">
+                        <div></div>
                     </div>
                 </div>
-            </div>
+            </main>
         </AuthenticatedLayout>
     );
 }
