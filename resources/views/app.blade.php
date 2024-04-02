bd627202a0a1daba0c04d9a78b261a73d8c3447f
Zoe31303133 <ch10161016@gmail.com>
2024-04-02 17:35:31 +0800

    引用Jquery, JqueryUI

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,8 +9,15 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <script
+			  src="https://code.jquery.com/jquery-3.7.1.js"
+			  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+			  crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
 
-        <!-- Scripts -->
+        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
+            <!-- Scripts -->
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
